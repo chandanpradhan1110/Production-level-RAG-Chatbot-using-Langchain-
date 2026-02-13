05917d65cdda82568f18620147be3035b26e4a10
chandanpradhan1110 <chandan.pradhan1110@gmail.com>
2026-02-13 12:18:32 +0530

    requirements.tx added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical-chatbot',
+    version='0.1.0',
+    author='Chandan Pradhan',
+    author_email = 'chandan.pradhan1110@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
+
